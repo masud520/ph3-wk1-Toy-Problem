95262c6497bc404dd9158a8f7917166ea9900f71
masud520 <masud.abdi@student.moringaschool.com>
2023-12-11 09:18:48 +0300

    added chllng2

---
+++ number.py
@@ -0,0 +1,14 @@
+def exactly_two_positive(a, b, c):
+    # Count the number of positive integers
+    positive_count = 0
+    if a > 0:
+        positive_count += 1
+    if b > 0:
+        positive_count += 1
+    if c > 0:
+        positive_count += 1
+    
+    # Return True if exactly two integers are positive
+    return positive_count == 2
+
+
