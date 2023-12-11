2107c3c1cf0409213118da5424477955f0010c78
masud520 <masud.abdi@student.moringaschool.com>
2023-12-11 09:20:13 +0300

    done with chllnge 2

--- number.py
+++ number.py
@@ -11,4 +11,10 @@ def exactly_two_positive(a, b, c):
     # Return True if exactly two integers are positive
     return positive_count == 2
 
-
+# Test cases
+print(exactly_two_positive(2, 4, -3))    # Output: True
+print(exactly_two_positive(-4, 6, 8))   # Output: True
+print(exactly_two_positive(4, -6, 9))   # Output: True
+print(exactly_two_positive(-4, 6, 0))   # Output: False
+print(exactly_two_positive(4, 6, 10))   # Output: False
+print(exactly_two_positive(-14, -3, -4))# Output: False
