65a48699330451279985189194bea38063021355
masud520 <masud.abdi@student.moringaschool.com>
2023-12-11 09:12:42 +0300

    working on  challenge 1

--- challenge1.py
+++ challenge1.py
@@ -4,3 +4,10 @@ def clock_converter(timestr):
     #handle 12am
     if time[:2]=="12"and time[-2:]=="am":
         return('00'+time[2:-2])
+    #handle 12pm
+    elif time[:2]=="12" and time[-2:]=="pm":
+            return(time[0:-2])
+    #handle am
+    elif time[-2:]=="am":
+        am_time=int(time.replace("am",""))
+        #handle if hour is a single digit
